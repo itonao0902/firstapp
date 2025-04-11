0d2a5e5c88204317649dee11e82a56fbccfe7a5b
AzureAD\itonao <ito.nao@blueship.co.jp>
2025-04-11 15:11:33 +0900

    新規投稿ページの実装

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 //サーバーサイドの処理を読み込み
 const allPosts = require('../controllers/posts/allPosts');
+const createGet = require('../controllers/posts/createGet');
 
 //リクエストに対する実行処理を定義
-router.get('/posts', allPosts);               //投稿一覧表示
+router.get('/posts', allPosts);               //投稿一覧表 
+router.get('/posts/create', createGet);       //投稿画面表示
 
 // ここでのルーターはexpress.Router(); 
 // module.exports = router;役目の認識 ここはルーターですよの意
